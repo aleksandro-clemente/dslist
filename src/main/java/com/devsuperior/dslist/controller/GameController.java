d1473f01bbf643b13edfca2edd417731e08cb732
Aleksandro Clemente <aleksandro1404@gmail.com>
2023-05-09 00:08:32 -0300

    Game repository,service,controller

---
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.model.Game;
+import com.devsuperior.dslist.service.GameService;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+	
+	@Autowired
+	private GameService service;
+	
+	@GetMapping
+	public ResponseEntity<List<GameMinDTO>> findAll() {
+		List<GameMinDTO> dto = service.findAll();
+		return ResponseEntity.ok().body(dto);
+	}
+}
