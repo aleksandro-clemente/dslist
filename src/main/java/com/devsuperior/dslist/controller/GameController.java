e39ffbd5cedbac32a2b6e97a5763c47ced6edc0f
Aleksandro Clemente <aleksandro1404@gmail.com>
2023-05-10 15:51:09 -0300

    Domain Model

--- src/main/java/com/devsuperior/dslist/controller/GameController.java
+++ src/main/java/com/devsuperior/dslist/controller/GameController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.model.Game;
 import com.devsuperior.dslist.service.GameService;
 
 @RestController
